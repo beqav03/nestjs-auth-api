1381cb8ffedac53b9f12d8c67132330d9c759d6d
BeqaV03 <b.vardy03@gmail.com>
2025-07-25 15:01:00 +0400

    feat: Implement user guard for role-based access control feat: Create refresh token repository for managing refresh tokens feat: Define user roles and statuses in enums feat: Add validators for email and password strength feat: Implement OTP verification DTO for resend functionality feat: Create OTP code entity for managing OTP codes feat: Develop OTP controller for handling OTP requests feat: Set up OTP module with necessary providers and controllers feat: Create OTP repository for managing OTP codes in the database feat: Implement OTP service for sending and verifying OTP codes feat: Add sanitize pipe for input validation and sanitization chore: Extend Express request types to include user information feat: Create user DTOs for user creation and updates feat: Implement update password DTO for user password management feat: Create update user DTO for user information updates feat: Define user entity with necessary fields and relationships feat: Develop users controller for managing user-related operations feat: Set up users module with necessary imports and providers feat: Create users repository for database operations related to users feat: Implement users service for business logic related to user management feat: Add utility functions for hashing and generating OTP codes

---
+++ src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { AuditLogModule } from 'src/audit/audit-log.module';
+import { OtpsModule } from 'src/otp/otp.module';
+
+@Module({
+  imports: [
+        TypeOrmModule.forFeature([User]),
+        AuditLogModule,
+        OtpsModule
+    ],
+    controllers: [UsersController],
+    providers: [UsersService, UsersRepository],
+    exports: [UsersService, UsersRepository],
+})
+export class UsersModule {}
